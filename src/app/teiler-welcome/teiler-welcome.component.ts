b995da0d99a7781c47cd0ba3fdebe229f01758aa
juarez <d.juarez@dkfz-heidelberg.de>
2022-10-05 11:04:02 +0200

    Revert "Add TeilerEnvironment"
    This reverts commit 4179e57170143023d5d1f9fc02bdd61054e8ec0a.

--- src/app/teiler-welcome/teiler-welcome.component.ts
+++ src/app/teiler-welcome/teiler-welcome.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {environment} from "../../environments/environment";
-import {EnvironmentService} from "../environment.service";
 
 class TeilerAdmin {
   name: string | undefined;
@@ -26,12 +25,10 @@ class TeilerAdmin {
 })
 export class TeilerWelcomeComponent implements OnInit {
 
-  teilerProject: string| undefined = undefined;
   teilerAdmin: TeilerAdmin = new TeilerAdmin();
   //TODO
-  welcomeTitle: string = "Welcome to your " + this.teilerProject + "Bridgehead";
   //welcomeTitle: string = "Welcome to your " + environment.config.TEILER_PROJECT + "Bridgehead";
-  //welcomeTitle: string = "Welcome to your DKTK Bridgehead";
+  welcomeTitle: string = "Welcome to your DKTK Bridgehead";
   welcomeMessage: string = `
   You are on the entry page of the Bridgehead on site DKTK Brückenkopf.
   The Bridgehead is used to convert a site's data into a DKTK-compatible format and make it usable for the other components.
@@ -42,8 +39,7 @@ export class TeilerWelcomeComponent implements OnInit {
   If you have any questions, please contact your local bridgehead administrator:
   `;
 
-  constructor(environmentService: EnvironmentService) {
-    environmentService.getEnvironmentSubscriber().subscribe(environment => {this.teilerProject = environment.teilerProject})
+  constructor() {
   }
 
   ngOnInit(): void {
