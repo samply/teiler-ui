644a59bece19c1a110ea0c428898c61618cd46f7
Fabian Schneider <fabianschneider2609@gmail.com>
2022-12-05 23:18:11 +0100

    renamed app script and added cli support

--- create_embedded_app.py
+++ create_embedded_app.py
@@ -1,17 +1,64 @@
 import subprocess
 from os import path
 import shutil
+import argparse
 
-# setup
-NAME = "mytest"
-DESCRIPTION = "my test description"
-ICON_CLASS = "bi bi-8-square-fill"
-ICON_SOURCE_URL = "undefined"
-TITLE = "My Test"
-ROLES = "TeilerRole.TEILER_PUBLIC"
+# Create the parser
+parser = argparse.ArgumentParser(prog="create-embedded-app",
+                                 description="Creates an embedded app in the Teiler-UI.")
+
+parser.add_argument("-n",
+                    dest="NAME",
+                    type=str,
+                    help="Name of the embedded app (required)",
+                    required=True)
+
+parser.add_argument("-t",
+                    dest="TITLE",
+                    type=str,
+                    default="",
+                    help="Title of the embedded app (default: '')")
+
+parser.add_argument("-r",
+                    dest="ROLES",
+                    type=str,
+                    nargs="+",
+                    default=['PUBLIC'],
+                    choices=['PUBLIC', 'USER', 'ADMIN'],
+                    help="Roles of the embedded app (default: PUBLIC)")
+
+parser.add_argument("-d",
+                    dest="DESCRIPTION",
+                    type=str,
+                    default="",
+                    help="Description of the embedded app (default: '')")
+
+group_icon = parser.add_mutually_exclusive_group(required=True)
+
+group_icon.add_argument("-ic",
+                        dest="ICON_CLASS",
+                        type=str,
+                        default="undefined",
+                        help="Icon class of the embedded app (default: undefined)")
+
+
+group_icon.add_argument("-is",
+                        dest="ICON_SOURCE_URL",
+                        type=str,
+                        default="undefined",
+                        help="URL of the icon for the embedded app (default: undefined)")
+
+args = parser.parse_args()
+
+# add quotation marks to the icon parameters, if they are defined
+if args.ICON_CLASS != "undefined":
+    args.ICON_CLASS = f"'{args.ICON_CLASS}'"
+if args.ICON_SOURCE_URL != "undefined":
+    args.ICON_SOURCE_URL = f"'{args.ICON_SOURCE_URL}'"
 
 root = path.dirname(path.abspath(__file__))
 
+# definition of all needed paths
 APP = path.join(root, "src", "app")
 APP_MODULES = path.join(APP, "app.module.ts")
 APP_ROUTING_MODULES = path.join(APP, "route", "app-routing.module.ts")
@@ -19,38 +66,39 @@ TEILER = path.join(APP, "teiler")
 TEILER_MODULES = path.join(TEILER, "teiler.module.ts")
 TEILER_APP = path.join(TEILER, "teiler-app.ts")
 TEILER_SERVICE = path.join(TEILER, "teiler.service.ts")
-SERVICE_FILE = path.join(APP, f"{NAME}.service.ts")
-SERVICE_SPEC_FILE = path.join(APP, f"{NAME}.service.spec.ts")
-COMPONENT_FILE = path.join(APP, NAME, f"{NAME}.component.ts")
+SERVICE_FILE = path.join(APP, f"{args.NAME}.service.ts")
+SERVICE_SPEC_FILE = path.join(APP, f"{args.NAME}.service.spec.ts")
+COMPONENT_FILE = path.join(APP, args.NAME, f"{args.NAME}.component.ts")
 ROUTE_MANAGER = path.join(APP, 'route', "route-manager.service.ts")
 
+# different variations of the NAME, that will be used later
+# component and service name will be extracted from the generated files
 COMPONENT_NAME = ""
 SERVICE_NAME = ""
-NAME_ENUM = NAME.replace("-", "_").upper()
-
+NAME_ENUM = args.NAME.replace("-", "_").upper()
 
 """------------------------------
 1. Generate service and component
 ------------------------------"""
-print(f"\nGenerate new service {NAME}...")
+print(f"\nGenerate new service {args.NAME}...")
 
-rt = subprocess.call(['npm', 'run', 'ng', 'g', 's', NAME], cwd=root, shell=True)
+rt = subprocess.call(['npm', 'run', 'ng', 'g', 's', args.NAME], cwd=root,
+                     shell=True)
 if rt:
     print("Generating failed...")
     exit()
 
-
-print(f"\nGenerate new component {NAME}...")
-rt = subprocess.call(['npm', 'run', 'ng', 'g', 'c', NAME], cwd=root, shell=True)
+print(f"\nGenerate new component {args.NAME}...")
+rt = subprocess.call(['npm', 'run', 'ng', 'g', 'c', args.NAME], cwd=root,
+                     shell=True)
 if rt:
     print("Something went wrong")
     exit()
 
-
 """-------------------------------------
 1.5. Remove Samply from XXX.component.ts
 -------------------------------------"""
-print(f"\nRemove Samply from {NAME}.component.ts")
+print(f"\nRemove Samply from {args.NAME}.component.ts")
 
 with open(COMPONENT_FILE, "r") as file:
     lines = file.readlines()
@@ -65,17 +113,15 @@ with open(COMPONENT_FILE, "r") as file:
 with open(COMPONENT_FILE, "w") as file:
     file.writelines(lines)
 
-
 """----------------------------------------------
 2. Move component directory to embedded directory
 ----------------------------------------------"""
 print(f"\nMoving component directory to embedded directory...")
 
-dest_dir = path.join(APP, "embedded", NAME)
+dest_dir = path.join(APP, "embedded", args.NAME)
 if path.isdir(dest_dir):
     shutil.rmtree(dest_dir)
-shutil.move(path.join(APP, NAME), path.join(APP, "embedded"))
-
+shutil.move(path.join(APP, args.NAME), path.join(APP, "embedded"))
 
 """---------------------------------
 3. Remove component in app.module.ts
@@ -89,7 +135,7 @@ import_string = ""
 with open(APP_MODULES, "r") as file:
     lines = file.readlines()
     for i, line in enumerate(lines):
-        if not found_section and line.find(NAME) != -1:
+        if not found_section and line.find(args.NAME) != -1:
             COMPONENT_NAME = line[line.find("{ ") + 2: line.find(" }")]
             SERVICE_NAME = COMPONENT_NAME.replace("Component", "Service")
             delete_lines.append(i)
@@ -108,7 +154,6 @@ with open(APP_MODULES, "r") as file:
 with open(APP_MODULES, "w") as file:
     file.writelines(lines)
 
-
 """--------------------------------------
 4. Add component to app-routing.module.ts
 --------------------------------------"""
@@ -124,7 +169,7 @@ with open(APP_ROUTING_MODULES, "r") as file:
             last_import_line = i
             continue
         if line.find("]") != -1:
-            lines[i-1] = lines[i-1].replace("\n", ",\n")
+            lines[i - 1] = lines[i - 1].replace("\n", ",\n")
             lines.insert(i, f"\t{COMPONENT_NAME}\n")
             lines.insert(last_import_line + 1, import_string)
             break
@@ -135,7 +180,6 @@ with open(APP_ROUTING_MODULES, "r") as file:
 with open(APP_ROUTING_MODULES, "w") as file:
     file.writelines(lines)
 
-
 """---------------------------------------
 5.	Move service files to teiler directory
 ---------------------------------------"""
@@ -144,13 +188,12 @@ print(f"\nMoving Service Files...")
 SERVICE_FILE = shutil.move(SERVICE_FILE, path.join(APP, "teiler"))
 SERVICE_SPEC_FILE = shutil.move(SERVICE_SPEC_FILE, path.join(APP, "teiler"))
 
-
 """--------------------------------------------
 6. Add service to providers in teiler.module.ts
 --------------------------------------------"""
 print(f"\nAdding service to providers in teiler.module.ts...")
 
-import_string = f'import {{{SERVICE_NAME}}} from "./{NAME}.service";\n'
+import_string = f'import {{{SERVICE_NAME}}} from "./{args.NAME}.service";\n'
 
 with open(TEILER_MODULES, "r") as file:
     lines = file.readlines()
@@ -170,7 +213,6 @@ with open(TEILER_MODULES, "r") as file:
 with open(TEILER_MODULES, "w") as file:
     file.writelines(lines)
 
-
 """---------------------------------------
 7. Add EmbeddedTeilerApps in teiler-app.ts
 ---------------------------------------"""
@@ -184,8 +226,8 @@ with open(TEILER_APP, "r") as file:
             found_section = True
             continue
         if found_section and line.find("}") != -1:
-            lines[i-1] = lines[i-1].replace("\n", ",\n")
-            lines.insert(i, f"\t{NAME_ENUM} = '{NAME}'\n")
+            lines[i - 1] = lines[i - 1].replace("\n", ",\n")
+            lines.insert(i, f"\t{NAME_ENUM} = '{args.NAME}'\n")
             break
     else:
         print("Could not find the needed lines.")
@@ -202,19 +244,19 @@ with open(TEILER_APP, "w") as file:
 print(f"\nExtending Service to EbeddedTeilerApp in XXX.service.ts...")
 
 template = \
-f'''\tdescription: string = "{DESCRIPTION}";
-\ticonClass: string = "{ICON_CLASS}";
-\ticonSourceUrl: string | undefined = {ICON_SOURCE_URL};
-\ttitle: string = "{TITLE}";
-\troles: TeilerRole[] = [{ROLES}];
+    f'''\tdescription: string = "{args.DESCRIPTION}";
+\ticonClass: string | undefined = {args.ICON_CLASS};
+\ticonSourceUrl: string | undefined = {args.ICON_SOURCE_URL};
+\ttitle: string = "{args.TITLE}";
+\troles: TeilerRole[] = [{",".join(["TeilerRole.TEILER_" + role for role in args.ROLES])}];
 
 \tconstructor(router: Router) {{
     super(EmbeddedTeilerApps.{NAME_ENUM}, router);
 \t}}\n'''
 
 import_string = \
-'''import {EmbeddedTeilerApp, EmbeddedTeilerApps, TeilerRole} from "./teiler-app";
-import {Router} from "@angular/router";\n'''
+    '''import {EmbeddedTeilerApp, EmbeddedTeilerApps, TeilerRole} from "./teiler-app";
+    import {Router} from "@angular/router";\n'''
 
 with open(SERVICE_FILE, "r") as file:
     lines = file.readlines()
@@ -235,13 +277,12 @@ with open(SERVICE_FILE, "r") as file:
 with open(SERVICE_FILE, "w") as file:
     file.writelines(lines)
 
-
 """--------------------------------
 9. Add service to teiler.service.ts
 --------------------------------"""
 print(f"\nAdding service to teiler.service.ts...")
 
-import_string = f'import {{{SERVICE_NAME}}} from "./{NAME}.service";\n'
+import_string = f'import {{{SERVICE_NAME}}} from "./{args.NAME}.service";\n'
 
 with open(TEILER_SERVICE, "r") as file:
     lines = file.readlines()
@@ -255,8 +296,8 @@ with open(TEILER_SERVICE, "r") as file:
             continue
         if found_section and line.find(")") != -1:
             SERVICE_NAME_lower = SERVICE_NAME[0].lower() + SERVICE_NAME[1:]
-            lines[i-1] = lines[i-1].replace("\n", ",\n")
-            lines[i+1] = lines[i+1].replace("]", f", {SERVICE_NAME_lower}]")
+            lines[i - 1] = lines[i - 1].replace("\n", ",\n")
+            lines[i + 1] = lines[i + 1].replace("]", f", {SERVICE_NAME_lower}]")
             lines.insert(i, f"\t\t{SERVICE_NAME_lower}: {SERVICE_NAME}\n")
             lines.insert(last_import_line + 1, import_string)
             break
@@ -272,7 +313,7 @@ with open(TEILER_SERVICE, "w") as file:
 ------------------------------------------"""
 print(f"\nAdding component to route-manager.service.ts...")
 
-import_string = f'import {{{COMPONENT_NAME}}} from "../embedded/{NAME}/{NAME}.component";\n'
+import_string = f'import {{{COMPONENT_NAME}}} from "../embedded/{args.NAME}/{args.NAME}.component";\n'
 template = f'\t\t{{name: EmbeddedTeilerApps.{NAME_ENUM}, component: {COMPONENT_NAME}}}\n'
 
 with open(ROUTE_MANAGER, "r") as file:
@@ -286,7 +327,7 @@ with open(ROUTE_MANAGER, "r") as file:
             found_section = True
             continue
         if found_section and line.find("].map") != -1:
-            lines[i-1] = lines[i-1].replace("\n", ",\n")
+            lines[i - 1] = lines[i - 1].replace("\n", ",\n")
             lines.insert(i, template)
             lines.insert(last_import_line + 1, import_string)
             break
